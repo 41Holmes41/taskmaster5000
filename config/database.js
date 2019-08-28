6a1c9916aa4e7812f4fc091e647f39599a7238e8
Cole Holmes <mitchellcholmes@gmail.com>
2019-08-28 16:56:57 -0500

    continues adding set-up

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.connect('process.env.DATABASE_URL',
+mongoose.connect(process.env.DATABASE_URL,
     {useNewUrlParser: true, useCreateIndex: true}
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var indexCtlr = require('../controllers/index')
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexCtlr.index);
+
+ // Google OAuth login route
+ router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+ // Google OAuth callback route
+ router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/students',
+    failureRedirect : '/students'
+  }
+));
 
 module.exports = router;
